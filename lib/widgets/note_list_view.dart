aa9b4f4727aad7576baa34c7d806571ca8653741
ibrahim ranjous <ibrahim.ranjous@gmail.com>
2024-04-17 20:45:18 +0300

    display notes part one & ( edit something in notes view body )

--- lib/widgets/note_list_view.dart
+++ lib/widgets/note_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/cubit/notes_cubit.dart';
+import 'package:notes/models/note_model.dart';
 import 'package:notes/widgets/note_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -6,16 +9,21 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        physics: const BouncingScrollPhysics(),
-        itemCount: 10,
-        itemBuilder: (context, Index) => const Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-          child: NoteItem(),
-        ),
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+        return Expanded(
+          child: ListView.builder(
+            itemCount: notes.length,
+            padding: EdgeInsets.zero,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, Index) => const Padding(
+              padding: EdgeInsets.symmetric(vertical: 4),
+              child: NoteItem(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
